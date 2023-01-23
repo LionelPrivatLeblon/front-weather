a5283d9064fdd693083331919f85464da780517d
Lionel <l.privatleblon@gmail.com>
2023-01-23 17:18:24 +0100

    Sauvegarde

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ document.querySelector("#connection").addEventListener("click", function () {
   };
   console.log(user);
 
-  fetch("http://localhost:3000/users/signin", {
+  fetch("https://front-weather.vercel.app/users/signin", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(user),
@@ -29,7 +29,7 @@ document.querySelector("#register").addEventListener("click", function () {
     password: document.querySelector("#registerPassword").value,
   };
 
-  fetch("http://localhost:3000/users/signup", {
+  fetch("https://front-weather.vercel.app/users/signup", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(user),
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:3000/weather")
+fetch("https://front-weather.vercel.app/weather")
   .then((response) => response.json())
   .then((data) => {
     if (data.weather) {
@@ -26,7 +26,9 @@ function updateDeleteCityEventListener() {
     document
       .querySelectorAll(".deleteCity")
       [i].addEventListener("click", function () {
-        fetch(`http://localhost:3000/weather/${this.id}`, { method: "DELETE" })
+        fetch(`https://front-weather.vercel.app/weather/${this.id}`, {
+          method: "DELETE",
+        })
           .then((response) => response.json())
           .then((data) => {
             if (data.result) {
@@ -40,7 +42,7 @@ function updateDeleteCityEventListener() {
 document.querySelector("#addCity").addEventListener("click", function () {
   const cityName = document.querySelector("#cityNameInput").value;
 
-  fetch("http://localhost:3000/weather", {
+  fetch("https://front-weather.vercel.app/weather", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ cityName }),
