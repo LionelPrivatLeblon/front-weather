acfa046461f3f82c223a03cf719ce3046f439c12
Lionel <l.privatleblon@gmail.com>
2023-01-23 17:04:55 +0100

    sauvegarde

---
+++ login.js
@@ -0,0 +1,43 @@
+// Insert your code here
+
+document.querySelector("#connection").addEventListener("click", function () {
+  const user = {
+    email: document.querySelector("#connectionEmail").value,
+    password: document.querySelector("#connectionPassword").value,
+  };
+  console.log(user);
+
+  fetch("http://localhost:3000/users/signin", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(user),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data", data);
+      if (data.result) {
+        window.location.assign("index.html");
+      }
+    })
+    .catch((error) => console.log(error));
+});
+
+document.querySelector("#register").addEventListener("click", function () {
+  const user = {
+    name: document.querySelector("#registerName").value,
+    email: document.querySelector("#registerEmail").value,
+    password: document.querySelector("#registerPassword").value,
+  };
+
+  fetch("http://localhost:3000/users/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(user),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.result) {
+        window.location.assign("index.html");
+      }
+    });
+});
